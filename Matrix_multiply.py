068b588930b922186f45feb8e3e5e237e8081490
OhMyCode13 <dirty13th@gmail.com>
2023-01-07 20:55:55 +0100

    Added the commentary

--- Matrix_multiply.py
+++ Matrix_multiply.py
@@ -1,4 +1,4 @@
-#import numpy as np
+# import numpy as np
 
 def matrix_mult(a, b):
     m = len(a)  # a: m × n
@@ -10,15 +10,15 @@ def matrix_mult(a, b):
             c[i][j] = sum(a[i][kk] * b[kk][j] for kk in range(n))
     return c
 
+
 #   return (np.matrix(a) * np.matrix(b)).tolist()
 
 
-m1= [ [1, 2, 3],
-    [3, 2, 1],
-    [2, 1, 3] ]
-m2 =[ [4, 5, 6],
-    [6, 5, 4],
-    [4, 6, 5] ]
+m1 = [[1, 2, 3],
+      [3, 2, 1],
+      [2, 1, 3]]
+m2 = [[4, 5, 6],
+      [6, 5, 4],
+      [4, 6, 5]]
 
-pprint(matrix_mult(m1,m2))
-
+print(matrix_mult(m1, m2))
